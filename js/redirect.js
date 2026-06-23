f09132db92339f2adbb3b2f42125e678b19643ad
adityapritam <adityapritam151@gmail.com>
2026-06-23 18:03:41 +0530

    changes

--- js/redirect.js
+++ js/redirect.js
@@ -10,3 +10,24 @@ let contact=document.getElementById("contact");
 contact.addEventListener("click",()=>{
     window.location.href = "../html/index.html#foot-1";
 });
+let about=document.getElementById("about");
+about.addEventListener("click",()=>{
+    console.log("loading");
+});
+
+document.getElementById("dhome").addEventListener("click",()=>{
+    home.click();
+    document.querySelector(".disappear").classList.remove("active");
+});
+document.getElementById("dbrowse").addEventListener("click",()=>{
+    browse.click();
+    document.querySelector(".disappear").classList.remove("active");
+});
+document.getElementById("dcontact").addEventListener("click",()=>{
+    contact.click();
+    document.querySelector(".disappear").classList.remove("active");
+});
+document.getElementById("dabout").addEventListener("click",()=>{
+    about.click();
+    document.querySelector(".disappear").classList.remove("active");
+});
