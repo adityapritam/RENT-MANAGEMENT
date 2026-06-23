c081e797086b04f811b372c7b40b0e925786049d
adityapritam <adityapritam151@gmail.com>
2026-06-23 15:19:51 +0530

    updated redirect.js

---
+++ js/redirect.js
@@ -0,0 +1,12 @@
+let home=document.getElementById("home");
+home.addEventListener("click",function(){
+    window.location.href = "../html/index.html#pgsection";
+});
+let browse=document.getElementById("browse");
+browse.addEventListener("click",()=>{
+    window.location.href = "../html/index.html#pgg-2";
+});
+let contact=document.getElementById("contact");
+contact.addEventListener("click",()=>{
+    window.location.href = "../html/index.html#foot-1";
+});
